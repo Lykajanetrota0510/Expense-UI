49069df92587f9d8813b7cc1acb644b11c6c82ab
Lykajanetrota0510 <80504300+Lykajanetrota0510@users.noreply.github.com>
2025-06-30 10:15:33 +0800

    Update api.js

---
+++ expense-ui/src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:7222/api/expenses/types/v1';
+
+export const getExpenseTypes = () => axios.get(`${API_URL}/GetAll`);
+export const createExpenseType = (data) => axios.post(`${API_URL}/Create`, data);
+export const updateExpenseType = (data) => axios.put(`${API_URL}/Update`, data);
+export const deleteExpenseType = (id) => axios.delete(`${API_URL}/Delete/${id}`);
